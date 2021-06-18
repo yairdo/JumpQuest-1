b15fca55dd040337cb8b2fce4cd3fc9de6bde82d
michall1900 <michall1900@gmail.com>
2021-06-18 21:33:42 +0300

    defewef

--- States/include/StandardMenuState.h
+++ States/include/StandardMenuState.h
@@ -6,10 +6,21 @@ class StateManger;
 
 class StandardMenuState : public MenuState {
 public:
+	using MenuState::MenuState;
 	virtual ~StandardMenuState()=default;
 
 protected:
-	sf::Vector2f m_middle;
+	template <class T>
+	void makeBut(sf::Vector2f& pos, int index, float height, float pix4let, double buttonSpace);
 
 };
+
+template <class T>
+void StandardMenuState::makeBut(sf::Vector2f& pos, int index, float height,
+	float pix4let, double buttonSpace) 
+{
+	float width = Resources::getResourceRef().getButLen(index) * pix4let;
+	addButton<T>(index, pos, width, height);
+	pos.y += buttonSpace + height;
+}
 
