f730fa1cfe843e8b9e3905218b8b6641dc38bbe6
michall1900 <michall1900@gmail.com>
2021-06-19 23:37:15 +0300

    merge again

--- include/Macros.h
+++ include/Macros.h
@@ -17,10 +17,7 @@ constexpr auto MAX_LIST_NAMES_SIZE=4;
 const float PLAYER_PROJECTILE_DIS = 300;
 const sf::Vector2f PROJECTILE_SIZE{20,20 };
 const sf::Vector2f ARCHER_SIZE{ 30,30 };
-enum MenuType {
-	mainMenu,
-	multiplayerMenu
-};
+
 
 
 enum direction {
