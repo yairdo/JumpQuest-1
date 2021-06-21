e1315cf53008ba517bd80668ff67d4b9f6fa067c
Shira Atwood <atwoodshira@gmail.com>
2021-06-21 23:33:27 +0300

    floor obstacle start

---
+++ include/FloorObstacle.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "MovingObj.h"
+#include "iostream"
+
+class b2Body;
+class b2World;
+//class sf::RenderWindow;
+
+class FloorObstacle : public MovingObj {
+public:
+	FloorObstacle(b2World&, const sf::Vector2f&, const sf::Vector2f&, const sf::Vector2f&, int, int);
+	virtual ~FallingBlock() = default;
+	FloorObstacle() = default;
+	virtual void updatePhysics(float);
+	virtual void move();
+	virtual void draw(sf::RenderWindow&);
+	virtual void updateAnim(float deltaTime);
+	virtual MovingObjInfo getInfo() const { return MovingObjInfo(getPos(), m_timer, m_body->GetLinearVelocity()); }
+	virtual void setInfo(MovingObjInfo info);
+	void setActiveAnim();
+	virtual void reset() override;
+
+private:
+	bool m_active = false;
+	float m_timer;
+	float m_startingTime;
+	static bool m_registerit;
+	sf::Vector2f m_size;
+
+};
