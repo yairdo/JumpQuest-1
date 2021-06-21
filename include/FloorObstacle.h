ac2f13fea2c9ab35eae2d5bf5db964a395aae6db
Shira Atwood <atwoodshira@gmail.com>
2021-06-22 00:20:57 +0300

    floor with bugs

--- include/FloorObstacle.h
+++ include/FloorObstacle.h
@@ -9,7 +9,7 @@ class b2World;
 class FloorObstacle : public MovingObj {
 public:
 	FloorObstacle(b2World&, const sf::Vector2f&, const sf::Vector2f&, const sf::Vector2f&, int, int);
-	virtual ~FallingBlock() = default;
+	virtual ~FloorObstacle() = default;
 	FloorObstacle() = default;
 	virtual void updatePhysics(float);
 	virtual void move();
@@ -17,11 +17,10 @@ public:
 	virtual void updateAnim(float deltaTime);
 	virtual MovingObjInfo getInfo() const { return MovingObjInfo(getPos(), m_timer, m_body->GetLinearVelocity()); }
 	virtual void setInfo(MovingObjInfo info);
-	void setActiveAnim();
 	virtual void reset() override;
 
 private:
-	bool m_active = false;
+	bool m_active;
 	float m_timer;
 	float m_startingTime;
 	static bool m_registerit;
