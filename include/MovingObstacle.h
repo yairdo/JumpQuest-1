398b5a4a8bede9158704785ce70c34301f9fa576
Eliad Karni <EliadK995@Gmail.com>
2021-06-24 22:07:27 +0300

    del

--- include/MovingObstacle.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "MovingObj.h"
-
-class MovingObstacle : public MovingObj {
-public:
-	virtual ~MovingObstacle() = default;
-	/*virtual void move();
-	virtual void handleCollision();*/
-};
--- include/StaticObstacle.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "StaticObj.h"
-class StaticObstacle : public StaticObj {
-public:
-	virtual ~StaticObstacle() = default;
-	//virtual void handleCollision();
-};
